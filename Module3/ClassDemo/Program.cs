72a780dabadd963a7c64e390dced5893821096f9
firebirdmanphoenix <phoenixtxv@gmail.com>
2025-10-18 16:25:35 -0700

    Completed

---
+++ Module3/ClassDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Author: Phoenix Vu
+Date: 10/18/2025
+Assignment: PA3
+*/
+
+
+namespace ClassDemo
+{
+    class Student
+    { 
+        //Fields
+        private string name;
+        private string address;
+        private int ID { get; set; }
+
+        //Method getter and setter
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string x)
+        {
+            name = x;
+        }
+
+        //Property getter and setter
+        public string SetAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        //Constructors
+
+        public Student() : this("John Doe", "308 Negra Arroyo Lane", 00000000) {}
+        public Student(string name) : this(name, "308 Negra Arroyo Lane", 00000000) {}
+
+        //Primary Constructor
+        public Student(string name, string address, int ID) 
+        {
+            this.name = name;
+            this.address = address;
+            this.ID = ID;
+        }
+
+        //PrintState
+        public void PrintState()
+        {
+            Console.WriteLine($"Name: {name}, Address: {address}, ID: {ID}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Student student1 = new Student("Arthur Morgan", "1984 Smth Street", 12345678);
+            student1.PrintState();
+
+            Student student2 = new Student("John Marston");
+            student2.PrintState();
+
+            Student student3 = new Student();
+            student3.PrintState();
+
+        }
+    }
+    
+}
---
+++ Module3/ExceptionDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Author: Phoenix Vu
+Date: 10/18/2025
+Assignment: PA3, Task 2
+*/
+namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetInt(5);
+                //Console.WriteLine(args[4]);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("dude you're dividing by 0");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error has occurred.");
+            }
+        }
+
+        static int GetInt(int x)
+        {
+            return (x/0);
+        }
+    }
+}
